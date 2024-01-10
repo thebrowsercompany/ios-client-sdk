3979699e055e2794390352b8cfd851e566868a4d
Brian Michel <brian.michel@gmail.com>
2024-01-10 16:21:53 -0500

    Go to event source revision with windows support

--- Package.swift
+++ Package.swift
@@ -2,25 +2,6 @@
 
 import PackageDescription
 
-func osSpecificPackageDependencies() -> [Package.Dependency] {
-    #if os(Linux) || os(Windows)
-    [
-        .package(url: "https://github.com/Quick/Quick.git", .exact("4.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .exact("9.2.1")),
-        .package(name: "LDSwiftEventSource", url: "https://github.com/brianmichel/swift-eventsource.git", .branch("brian/add-windows-support")),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
-    ]
-    #else
-    [
-        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .exact("9.1.0")),
-        .package(url: "https://github.com/Quick/Quick.git", .exact("4.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .exact("9.2.1")),
-        .package(name: "LDSwiftEventSource", url: "https://github.com/brianmichel/swift-eventsource.git", .branch("brian/add-windows-support")),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
-    ]
-    #endif
-}
-
 let package = Package(
     name: "LaunchDarkly",
     platforms: [
@@ -38,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .exact("9.1.0")),
         .package(url: "https://github.com/Quick/Quick.git", .exact("7.3.0")),
         .package(url: "https://github.com/Quick/Nimble.git", .exact("13.0.0")),
-        .package(name: "LDSwiftEventSource", url: "https://github.com/LaunchDarkly/swift-eventsource.git", .branch("brian/add-windows-support")),
+        .package(name: "LDSwiftEventSource", url: "https://github.com/LaunchDarkly/swift-eventsource.git", .revisionItem("ac5f18c")),
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
     ],
     targets: [
